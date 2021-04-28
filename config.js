2598ff714b3f2a47340b1fbcc0e277b17f010855
MiguelED <migueled2695@gmail.com>
2021-04-27 23:41:51 -0500

    Fetch

--- config.js
+++
@@ -1,6 +0,0 @@
-const keyWeatherStack = () =>'f56dfecb23b087a510db9c33afa1acac';
-const keyMapBox = () => 'pk.eyJ1IjoibWlndWVsZWQiLCJhIjoiY2tuczNmNDc3MDBkMDJ2bGhuMWFxcHdibyJ9.IRRjQ6hIfNHk3RehrZV0tw';
-module.exports = {
-    keyWeatherStack : keyWeatherStack,
-    keyMapBox : keyMapBox
-};
