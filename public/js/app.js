2fd585a85f48db26344b26266a96721dcd004c3d
MiguelED <migueled2695@gmail.com>
2021-04-28 00:23:37 -0500

    Creating a search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,24 @@
-fetch( 'http://localhost:3000/weather?address=boston' ).then( ( response ) => {
-    response.json().then( (data) => {
-        if( data.error ){
-            console.log( data.error );
-        } else {
-            console.log( data );
-        }
+const weatherForm = document.querySelector( 'form' );
+const search = document.querySelector( 'input' );
+
+weatherForm.addEventListener( 'submit' , ( e ) => {
+    e.preventDefault();
+    const location = search.value;
+    if( location ) {
+        getData( location );
+    } else {
+        console.log( 'else' );
+    }
+});
+
+const getData = ( location ) => {
+    fetch( `http://localhost:3000/weather?address=${ location }` ).then( ( response ) => {
+        response.json().then( (data) => {
+            if( data.error ){
+                console.log( data.error );
+            } else {
+                console.log( data );
+            }
+        })
     })
-})
+}
