2553af10496072600898127b9be9b2d080a494af
MiguelED <migueled2695@gmail.com>
2021-04-28 17:12:18 -0500

    Wiring up the user interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,15 @@
 const weatherForm = document.querySelector( 'form' );
 const search = document.querySelector( 'input' );
+const messageSecond = document.querySelector( '#message-1' )
 
 weatherForm.addEventListener( 'submit' , ( e ) => {
     e.preventDefault();
     const location = search.value;
     if( location ) {
+        messageSecond.textContent = 'Loading';
         getData( location );
     } else {
-        console.log( 'else' );
+        messageSecond.textContent = 'There is no data';
     }
 });
 
@@ -15,9 +17,9 @@ const getData = ( location ) => {
     fetch( `http://localhost:3000/weather?address=${ location }` ).then( ( response ) => {
         response.json().then( (data) => {
             if( data.error ){
-                console.log( data.error );
+                messageSecond.textContent = 'Try again later';
             } else {
-                console.log( data );
+                messageSecond.textContent = `${ data.location }, Feelslike: ${ data.forecast.feelslike }, Temperature: ${ data.forecast.temperature }, Weather Description: ${ data.forecast.weather_descriptions }`;
             }
         })
     })
