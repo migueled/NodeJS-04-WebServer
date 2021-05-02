deee9dbda2dca56f2cd68e9d0118de7a8bb4dfd2
MiguelED <migueled2695@gmail.com>
2021-05-02 04:29:59 -0500

    npm install dotenv

--- src/copy_config.js
+++ src/copy_config.js
@@ -1,5 +1,5 @@
-const keyWeatherStack = 'f56dfecb23b087a510db9c33afa1acac';
-const keyMapBox = 'pk.eyJ1IjoibWlndWVsZWQiLCJhIjoiY2tuczNmNDc3MDBkMDJ2bGhuMWFxcHdibyJ9.IRRjQ6hIfNHk3RehrZV0tw';
+const keyWeatherStack = '';
+const keyMapBox = '';
 
 const config = {
     keyWeatherStack,
