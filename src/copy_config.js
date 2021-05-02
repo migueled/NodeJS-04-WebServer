a3a1a28d556866e6fba12de83b848a36e025e8b1
MiguelED <migueled2695@gmail.com>
2021-05-02 04:27:18 -0500

    Config copy edit

--- src/copy_config.js
+++ src/copy_config.js
@@ -1,6 +1,15 @@
-const keyWeatherStack = () =>'';
-const keyMapBox = () => '';
-module.exports = {
-    keyWeatherStack : keyWeatherStack,
-    keyMapBox : keyMapBox
+const keyWeatherStack = 'f56dfecb23b087a510db9c33afa1acac';
+const keyMapBox = 'pk.eyJ1IjoibWlndWVsZWQiLCJhIjoiY2tuczNmNDc3MDBkMDJ2bGhuMWFxcHdibyJ9.IRRjQ6hIfNHk3RehrZV0tw';
+
+const config = {
+    keyWeatherStack,
+    keyMapBox,
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'http://localhost',
+    publicPath: process.env.PUBLIC_PATH || 'public',
+    templatesPath: process.env.PUBLIC_TEMPLATES || 'templates',
+    viewsPath: process.env.PUBLIC_VIEWS || 'views',
+    partialsPath: process.env.PUBLIC_PARTIALS || 'partials'
 };
+
+module.exports = config
